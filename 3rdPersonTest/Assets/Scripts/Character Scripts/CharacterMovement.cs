6bc8014d4ea023301e695ea175059ae2eeff27ce
ruigames <rui_games@hotmail.com>
2015-04-25 21:39:52 +0100

    Terrain and character controller modified and more

--- 3rdPersonTest/Assets/Scripts/Character Scripts/CharacterMovement.cs
+++ 3rdPersonTest/Assets/Scripts/Character Scripts/CharacterMovement.cs
@@ -3,29 +3,39 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 
-	public float speed;
-	// Use this for initialization
+	public float speed = 15.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+
+	private CharacterController controller ;
+	private Vector3 moveDirection = Vector3.zero;
+
+	
 	void Start () {
-
+		controller = GetComponent<CharacterController> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		float movY= Input.GetAxis("Vertical");
-		float movX = Input.GetAxis("Horizontal");
+	void Update() {
 
-		Vector3 move=new Vector3(-movX ,0, -movY);
-		// dont worry about a jump right now.
-	
-		Vector3 pos = new Vector3 (Camera.main.transform.position.x,0,Camera.main.transform.position.z);
+		if (controller.isGrounded) {
 
-		transform.LookAt(pos);
-		transform.Translate(move * speed * Time.deltaTime);
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+			
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+		Vector3 CameraDir = new Vector3 (Camera.main.transform.position.x,0,Camera.main.transform.position.z);
+		transform.LookAt(transform.position + CameraDir);
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if(other.tag=="Enemy1"||other.tag=="Enemy2")
-		Destroy(other.gameObject);
+		Destroy(other.transform.parent.gameObject);
 	}
 }
+
---
+++ 3rdPersonTest/Assets/Scripts/Enemy Script/EnemyName.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyName : MonoBehaviour {
+
+	public Transform enemy; //enemy's transform
+	public Vector3 positionOffset = Vector3.up; //offset relative to enemy's origin, so it doesn't, say, draw the text at the enemy's feet
+
+	// Use this for initialization
+	public void Start() {
+		transform.position = enemy.position + positionOffset;
+		transform.LookAt (Camera.main.transform);
+		//transform.localEulerAngles = new Vector3 (180, 0, 0);
+		//transform.parent = enemy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = enemy.position + positionOffset;
+
+	}
+}
