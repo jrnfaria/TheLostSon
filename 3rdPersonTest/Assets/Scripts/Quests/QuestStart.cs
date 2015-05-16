155bc99564952106846f4d554b52c609bdc56e4c
ei12159 <ei12159@fe.up.pt>
2015-05-16 23:09:27 +0100

    Added quest viewer. Static data for now

--- 3rdPersonTest/Assets/Scripts/Quests/QuestStart.cs
+++ 3rdPersonTest/Assets/Scripts/Quests/QuestStart.cs
@@ -2,31 +2,39 @@
 using System.Collections;
 
 public class QuestStart : MonoBehaviour {
-	private bool isInside =false;
+	private bool isInside = false;
+	private bool displayedGUI = false;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.F) && isInside) {
-			Debug.Log("OLA");
+			displayedGUI = true;
+		} else if (!isInside) {
+			displayedGUI=false;
 		}
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.name == "Tyson") {
-			Debug.Log("Entrei no COLLIDER");
 			isInside = true;
 		}
 	}
 
 	void OnTriggerExit(Collider other){
 		if (other.name == "Tyson") {
-			Debug.Log("Sai do COLLIDER");
 			isInside = false;
 		}
+	}
+
+	public bool isDisplayedGUI(){
+		return displayedGUI;
+	}
+
+	public void setDisplayedGUI(bool displayedGUI){
+		this.displayedGUI = displayedGUI;
 	}
 }
