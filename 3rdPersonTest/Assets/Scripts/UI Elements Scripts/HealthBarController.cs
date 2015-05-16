c2a3bdf86efbe19326d855825a333567b7cfabf6
ruigames <rui_games@hotmail.com>
2015-05-16 18:01:03 +0100

    Life bar above enemy

--- 3rdPersonTest/Assets/Scripts/UI Elements Scripts/HealthBarController.cs
+++ 3rdPersonTest/Assets/Scripts/UI Elements Scripts/HealthBarController.cs
@@ -3,34 +3,38 @@ using System.Collections;
 
 public class HealthBarController : MonoBehaviour {
 
-	private GameObject greenBar;
+	//private GameObject greenBar;
 	private GameObject redBar;
 	public GameObject enemyBody;
 	private EnemyHealth enemyHealth;
+
+	private float healthSize;
 
 	public Vector3 offset;
 
 
 
 	void Start () {
-		greenBar = transform.FindChild ("Green").gameObject;
+		//greenBar = transform.FindChild ("Green").gameObject;
 		redBar = transform.FindChild ("Red").gameObject;
 		enemyHealth = enemyBody.GetComponent<EnemyHealth> ();
+		healthSize = redBar.transform.localScale.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Transform cam = Camera.main.transform;
-		//Debug.Log (enemyHealth.getHealthRatio());
+	
+
+		//greenBar.transform.position = enemyBody.transform.position + offset;
 		redBar.transform.position = enemyBody.transform.position+offset;
+	
 
+		//greenBar.transform.localScale = new Vector3 (healthSize*enemyHealth.getHealthRatio(),greenBar.transform.localScale.y,greenBar.transform.localScale.z);
+		//redBar.transform.position -= new Vector3 (healthSize*enemyHealth.getHealthRatio()/2f,0,0);
 
-		greenBar.transform.position = enemyBody.transform.position+offset;
-		greenBar.transform.localScale = new Vector3 (greenBar.transform.localScale.x,1,greenBar.transform.localScale.z*enemyHealth.getHealthRatio());
-
+		
+		//greenBar.transform.LookAt (cam);
 		redBar.transform.LookAt (cam);
-
-		greenBar.transform.LookAt (cam,new Vector3(1,0,0));
-
 	}
 }
