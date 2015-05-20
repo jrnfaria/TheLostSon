2b7e31c5eb2d7560ee970402d1e5832855494113
ruigames <rui_games@hotmail.com>
2015-05-20 14:12:50 +0100

    created minimap

---
+++ 3rdPersonTest/Assets/Scripts/GUI/MinimapController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapController : MonoBehaviour {
+
+	private bool visible;
+	public GameObject miniCamera;
+
+	void Start () {
+		visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyUp(KeyCode.M))
+		{
+			visible=!visible;
+			miniCamera.SetActive(visible);
+		}
+
+	}
+}
