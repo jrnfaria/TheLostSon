45f2b023855fb9783df6c2aff77074e0c0a5514a
ruigames <rui_games@hotmail.com>
2015-06-12 20:49:30 +0100

    cave prototype

---
+++ 3rdPersonTest/Assets/Scripts/gate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gate : MonoBehaviour {
+
+	public int dimension;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if(other.transform.tag=="Character")
+		Application.LoadLevel (dimension);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
