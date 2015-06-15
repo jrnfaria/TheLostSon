9458a3017a94c8a6db81728d1cae2ebb61882386
ei12159 <ei12159@fe.up.pt>
2015-06-15 21:33:24 +0100

    fixed BigJump

--- 3rdPersonTest/Assets/Scripts/Character Scripts/CharacterMovement.cs
+++ 3rdPersonTest/Assets/Scripts/Character Scripts/CharacterMovement.cs
@@ -2,26 +2,26 @@
 using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
-
+	
 	public float speed = 15.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	public Animator anim;
-
+	
 	private CharacterController controller ;
 	private Vector3 moveDirection = Vector3.zero;
-
-
+	
+	
 	
 	void Start () {
 		controller = GetComponent<CharacterController> ();
 		anim= GetComponent<Animator> ();
-
-
+		
+		
 	}
-
+	
 	void Update() {
-
+		
 		anim.SetInteger("move",0);
 		if (controller.isGrounded) {
 			if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||Input.GetKey(KeyCode.D)){
@@ -36,17 +36,18 @@ public class CharacterMovement : MonoBehaviour {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
+			
+			if (Input.GetButton ("Jump") && anim.GetInteger("move")==2) {
+				anim.SetInteger ("move", 4);//BigJump
+			}else if (Input.GetButton ("Jump") && (anim.GetInteger("move")==0 || anim.GetInteger("move")==1)) {
+				anim.SetInteger ("move", 3);//jump
+			}
 		}
-		if (Input.GetButton ("Jump") && anim.GetInteger("move")==2) {
-			anim.SetInteger ("move", 4);//BigJump
-		}else if (Input.GetButton ("Jump")) {
-			anim.SetInteger ("move", 3);//jump
-		}
-
+		
 		moveDirection.y -= gravity * Time.deltaTime;
-
+		
 		controller.Move(moveDirection * Time.deltaTime);
-
+		
 		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
 	}
 	
