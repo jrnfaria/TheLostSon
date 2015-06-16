04eb9ecfa9c4cf565235531e88d08817810151ee
ruigames <rui_games@hotmail.com>
2015-06-16 11:46:25 +0100

    Some villegers and minor corrections

---
+++ 3rdPersonTest/Assets/Scripts/Villagers/VillagerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillagerMovement : MonoBehaviour {
+
+	private bool inDestiny;
+	private Vector3 initialPosition;
+	private NavMeshAgent nav; 
+	private Animator anim;
+	private Vector3 patrolPosition;
+
+	public int patrolRange;
+
+
+	void Start () {
+		inDestiny = false;
+		initialPosition = transform.position;
+		nav = GetComponent<NavMeshAgent>();
+		anim=GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inDestiny) {
+			CalculatePatrolPoint ();
+		} 
+		if (nav.remainingDistance == 0)
+			inDestiny = true;
+
+	}
+
+	void CalculatePatrolPoint()
+	{
+		Vector3 randomDirection = Random.insideUnitSphere * patrolRange;
+		
+		randomDirection += initialPosition;
+		NavMeshHit hit;
+		NavMesh.SamplePosition(randomDirection, out hit, patrolRange, 1);
+		patrolPosition = hit.position;
+		
+		inDestiny=false;
+		nav.destination = patrolPosition;
+	}
+}
