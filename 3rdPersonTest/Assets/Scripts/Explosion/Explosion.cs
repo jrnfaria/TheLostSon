8c434406e6ec16da64c08e12799123d4aa250139
ei12159 <ei12159@fe.up.pt>
2015-06-16 22:10:04 +0100

    Explosive minion now explodes xD...merge

---
+++ 3rdPersonTest/Assets/Scripts/Explosion/Explosion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	
+	public GameObject explosionPrefab;
+	public float fuseTime;
+	private bool explode=false;
+	
+	void Start() {
+	}
+	
+	void Update() {
+	}
+	
+	void Explode() {
+		if(explode){
+			Instantiate (explosionPrefab, this.transform.position, Quaternion.identity);
+			Destroy (gameObject);
+			Destroy (transform.parent.gameObject, 0.2f);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "Tyson") {
+			explode=true;
+			CancelInvoke("Explode");
+			Invoke("Explode", fuseTime);
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other.name == "Tyson") {
+			explode=false;
+		}
+	}
+}
