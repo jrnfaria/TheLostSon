c33b16b737a2599d4ab63e91c3b8bc43b3b977bd
ruigames <rui_games@hotmail.com>
2015-06-16 16:12:36 +0100

    Caves

--- 3rdPersonTest/Assets/Scripts/gate.cs
+++ 3rdPersonTest/Assets/Scripts/gate.cs
@@ -4,15 +4,28 @@ using System.Collections;
 public class gate : MonoBehaviour {
 
 	public int dimension;
+	public Vector3 beginPosition;
+	private GameObject HUD;
+	private GameObject inventory;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		HUD = GameObject.FindGameObjectWithTag ("HUD");
+		inventory = GameObject.FindGameObjectWithTag ("Inventory");
 	}
 
 
 	void OnTriggerEnter(Collider other) {
-		if(other.transform.tag=="Character")
-		Application.LoadLevel (dimension);
+		if (other.transform.tag == "Character") {
+			DontDestroyOnLoad(other.transform.gameObject);
+			DontDestroyOnLoad(HUD);
+			DontDestroyOnLoad(inventory);
+			DontDestroyOnLoad(Camera.main.gameObject);
+			other.transform.position=beginPosition;
+			Application.LoadLevel (dimension);
+		}
+
 	}
 
 
