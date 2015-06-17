156eaf251c954ddb3c6ae87d23521017039b50bb
ruigames <rui_games@hotmail.com>
2015-06-17 15:36:33 +0100

    Caves everywhere

--- 3rdPersonTest/Assets/Scripts/Enemy Script/EnemyHealth.cs
+++ 3rdPersonTest/Assets/Scripts/Enemy Script/EnemyHealth.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
 	public int fullHealth;
 	public int health;
 	private float healthRatio;
+	public Slider healthSlider;
+	public Text healthInfo;
 
 	void Start ()
 	{
 		healthRatio = 1f;
+		if (healthSlider != null) {
+			healthSlider.maxValue=fullHealth;
+			healthSlider.value=health;
+			healthInfo.text=fullHealth+"/"+health;
+		}
 	}
 
 	public float getHealthRatio()
@@ -21,13 +29,24 @@ public class EnemyHealth : MonoBehaviour
 	{
 		health -= dmg;
 
+		if (healthSlider != null) {
+			healthSlider.value=health;
+			healthInfo.text=health+"/"+fullHealth;
+		}
+
 		if (health <= 0) {
+			if (healthSlider != null) {
+				Destroy (healthSlider.gameObject);
+
+			}
+
 			Destroy (this.transform.parent.gameObject);
 			GameObject.FindGameObjectWithTag("Character").GetComponent<CharacterStatus>().addExp(10);
 			KillQuest quest = GameObject.FindGameObjectWithTag("Character").GetComponent<KillQuest>();
 			if(quest.getNumber() !=0 && quest.getMonster()==gameObject.name){
 				quest.setNumber(quest.getNumber()-1);
 			}
+
 		}
 
 		healthRatio = health / (float)fullHealth;
