6763722b4a14fc00799017e4c963174d1cc69c09
ruigames <rui_games@hotmail.com>
2015-06-18 19:55:45 +0100

    crash between scenes gone

--- 3rdPersonTest/Assets/Scripts/gate.cs
+++ 3rdPersonTest/Assets/Scripts/gate.cs
@@ -4,15 +4,31 @@ using System.Collections;
 public class gate : MonoBehaviour {
 
 	public int dimension;
+	public Vector3 beginPosition;
+
+	private GameObject HUD;
+	private GameObject inventory;
+	private GameObject Tyson;
+
 
 	// Use this for initialization
 	void Start () {
+		HUD = GameObject.FindGameObjectWithTag ("HUD");
+		inventory = GameObject.FindGameObjectWithTag ("Inventory");
+		Tyson= GameObject.FindGameObjectWithTag ("Character");
+
 	}
 
 
 	void OnTriggerEnter(Collider other) {
 		if (other.transform.tag == "Character") {
+			DontDestroyOnLoad(HUD);
+			DontDestroyOnLoad(inventory);
+			DontDestroyOnLoad(Tyson);
+			DontDestroyOnLoad(Camera.main);
+
 			Application.LoadLevel (dimension);
+			Tyson.transform.position=beginPosition;
 
 		}
 
