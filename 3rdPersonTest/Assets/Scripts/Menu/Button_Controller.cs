cfa3aff160941d9b52a9ad13ffd046cd4ec128be
ei12159 <ei12159@fe.up.pt>
2015-06-20 15:11:15 +0100

    some fixes on menu

--- 3rdPersonTest/Assets/Scripts/Menu/Button_Controller.cs
+++ 3rdPersonTest/Assets/Scripts/Menu/Button_Controller.cs
@@ -14,6 +14,9 @@ public class Button_Controller : MonoBehaviour {
 	public void Start_Game(){
 		menu.SetActive (false);
 		loading.SetActive (true);
-		Application.LoadLevelAsync(1);
+		//Application.LoadLevelAsync(1);
+		if(Application.GetStreamProgressForLevel(1) ==1){//0&&1 aproveitar para fazer progress bar
+			Application.LoadLevelAsync(1);
+		}
 	}
 }
