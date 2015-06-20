c3b2cb16a2047e9ea56b128d63a0c58e65476dd4
ei12159 <ei12159@fe.up.pt>
2015-06-20 22:28:43 +0100

    improved loading screen

--- 3rdPersonTest/Assets/Scripts/Menu/Button_Controller.cs
+++ 3rdPersonTest/Assets/Scripts/Menu/Button_Controller.cs
@@ -3,20 +3,11 @@ using System.Collections;
 
 public class Button_Controller : MonoBehaviour {
 
-	//Objects
-	public GameObject menu;
-	public GameObject loading;
-
 	public void Exit_Game(){
 		Application.Quit ();
 	}
 
 	public void Start_Game(){
-		menu.SetActive (false);
-		loading.SetActive (true);
-		//Application.LoadLevelAsync(1);
-		if(Application.GetStreamProgressForLevel(1) ==1){//0&&1 aproveitar para fazer progress bar
-			Application.LoadLevelAsync(1);
-		}
+		Application.LoadLevel(4);
 	}
 }
---
+++ 3rdPersonTest/Assets/Scripts/Menu/Load.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevelAsync (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
