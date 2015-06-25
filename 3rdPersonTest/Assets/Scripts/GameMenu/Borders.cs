4df5aa1eba67fe5444a9612a911de1f28dacff17
ruigames <rui_games@hotmail.com>
2015-06-25 20:58:50 +0100

    minmapa done

---
+++ 3rdPersonTest/Assets/Scripts/GameMenu/Borders.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Borders : MonoBehaviour {
+
+	public Text text;
+	public string name;
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Character")
+			text.text = name;
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Character")
+			text.text = "Wilderness";
+	}
+}
