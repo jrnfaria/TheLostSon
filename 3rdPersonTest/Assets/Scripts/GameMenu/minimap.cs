1a382190c82ac74f4726ef9b498a98ac48771920
ruigames <rui_games@hotmail.com>
2015-06-25 20:41:27 +0100

    minimap improved

--- 3rdPersonTest/Assets/Scripts/GameMenu/minimap.cs
+++ 3rdPersonTest/Assets/Scripts/GameMenu/minimap.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class minimap : MonoBehaviour {
 
-	public int xPos,yPos,width,height;
 	private float ratioX,ratioY,ratio;
 	// Use this for initialization
 	void Start () {
@@ -17,11 +16,10 @@ public class minimap : MonoBehaviour {
 		ratioX = Screen.width/1920f;
 		ratioY = Screen.height/1080f;
 		ratio = Camera.main.aspect;
-
-		Debug.Log (ratio);
+	
 
 		if(ratio>=1.7)
-			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 264*ratioX, Screen.height - 302*ratioY, 223*ratioX, 222*ratioY);
+			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 270*ratioX, Screen.height - 295*ratioY, 223*ratioX, 222*ratioY);
 		else if(ratio>=1.5)
 			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 280*ratioX, Screen.height - 293*ratioY, 230*ratioX, 222*ratioY);
 		else if(ratio>=1.33||ratio>=1.25)
