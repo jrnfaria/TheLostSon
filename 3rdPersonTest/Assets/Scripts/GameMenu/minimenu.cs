d1419e9bcde80c704ff59273d73e51b729a1abb2
ei12159 <ei12159@fe.up.pt>
2015-06-26 15:00:46 +0100

    minimenu resize fixes

--- 3rdPersonTest/Assets/Scripts/GameMenu/minimenu.cs
+++ 3rdPersonTest/Assets/Scripts/GameMenu/minimenu.cs
@@ -9,6 +9,8 @@ public class minimenu : MonoBehaviour {
 	Matrix4x4 matrix;
 	public GUISkin customSkin;
 
+	public Texture aTexture;
+
 	// Use this for initialization
 	void Start () {
 		matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity,new  Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
@@ -22,24 +24,26 @@ public class minimenu : MonoBehaviour {
 	void OnGUI(){
 		GUI.matrix = matrix;
 		GUI.skin = customSkin;
+		
+		GUI.DrawTexture(new Rect(1590, 850, 300, 300), aTexture);
 
-		if (GUI.Button (new Rect (1627, 945, 43, 90), "")) {//character
+		if (GUI.Button (new Rect (1605, 955, 50, 80), "")) {//character
 			Debug.Log("character");
 			CharacterMenu cm = gameObject.GetComponent<CharacterMenu>();
 			cm.show=!cm.show;
-		}else if (GUI.Button (new Rect (1675, 945, 43, 90), "")) {//inventory
+		}else if (GUI.Button (new Rect (1660, 955, 50, 80), "")) {//inventory
 			Debug.Log("inventory");
 			Inventory inv = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
 			inv.show=!inv.show;
-		}else if (GUI.Button (new Rect (1723, 945, 43, 90), "")) {//status
+		}else if (GUI.Button (new Rect (1715, 955, 50, 80), "")) {//status
 			Debug.Log("status");
 			StatusMenu sm = gameObject.GetComponent<StatusMenu>();
 			sm.show=!sm.show;
-		}else if (GUI.Button (new Rect (1771, 945, 43, 90), "")) {//quests
+		}else if (GUI.Button (new Rect (1770, 955, 50, 80), "")) {//quests
 			Debug.Log("quests");
 			QuestViewer qview = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<QuestViewer>();
 			qview.setShowInfoQuestMenu();
-		}else if (GUI.Button (new Rect (1819, 945, 43, 90), "")) {//settings
+		}else if (GUI.Button (new Rect (1825, 955, 50, 80), "")) {//settings
 			Debug.Log("settings");
 			ExitMenu em = gameObject.GetComponent<ExitMenu>();
 			em.show=!em.show;
