14d56d48eddf19035f11975569b7fbb4171458c0
ei12159 <ei12159@fe.up.pt>
2015-06-26 21:45:59 +0100

    minimap works finally

--- 3rdPersonTest/Assets/Scripts/GameMenu/minimap.cs
+++ 3rdPersonTest/Assets/Scripts/GameMenu/minimap.cs
@@ -2,27 +2,25 @@
 using System.Collections;
 
 public class minimap : MonoBehaviour {
+	
+	public Texture aTexture;
+	public RenderTexture minimapTexture;
+	public Material minimapMaterial;
 
-	private float ratioX,ratioY,ratio;
-	// Use this for initialization
-	void Start () {
-		ratioX = Screen.width/1920f;
-		ratioY = Screen.height/1080f;
-		ratio = Camera.main.aspect;
+	private float virtualWidth = 1920.0f;
+	private float virtualHeight = 1080.0f;
+	private Matrix4x4 matrix;
+
+	void Awake(){
+		matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity,new  Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
+	}
+
+	void OnGUI(){
+		GUI.matrix = matrix;
+
+		if (Event.current.type == EventType.Repaint) {
+			Graphics.DrawTexture (new Rect (1615, 90, 225, 250), minimapTexture, minimapMaterial);
 		}
-	
-	// Update is called once per frame
-	void Update () {
-		ratioX = Screen.width/1920f;
-		ratioY = Screen.height/1080f;
-		ratio = Camera.main.aspect;
-	
-
-		if(ratio>=1.7)
-			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 270*ratioX, Screen.height - 295*ratioY, 223*ratioX, 222*ratioY);
-		else if(ratio>=1.5)
-			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 280*ratioX, Screen.height - 293*ratioY, 230*ratioX, 222*ratioY);
-		else if(ratio>=1.33||ratio>=1.25)
-			transform.GetComponent<Camera>().pixelRect = new Rect(Screen.width - 311*ratioX, Screen.height - 262*ratioY, 253*ratioX, 200*ratioY);
+		GUI.DrawTexture(new Rect(1495, 0, 455, 450), aTexture);
 	}
 }
